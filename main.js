4865fb06179eceaf93e827fea0691589450e947f
EsperCode404 <akshitkumar4266@gmail.com>
2026-06-22 01:43:06 +0530

    feat: design alignment and desktop engine configuration

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 390,
+        height: 844,
+        useContentSize: true,
+        title: "SYSTEM TERMINAL // Cyberpunk Dashboard",
+        backgroundColor: '#000000',
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            backgroundThrottling: false,
+        }
+    });
+
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
